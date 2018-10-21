3c313b8dc02d20e5e5840cfed9c6ac8b6cad7061
Ayanami_Asuka <fyb0118@gmail.com>
2018-10-21 16:29:37 +0800

    将处理文章具体内容的视图函数用类视图替换

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post,Category
 from comments.forms import CommentForm
 from django.views.generic import ListView
+from django.views.generic import DetailView
 import markdown
 import pygments
 # Create your views here.
@@ -19,25 +20,71 @@ class IndexView(ListView):
     template_name='blog/index.html'
     context_object_name='post_list'
 
-def detail(request,pk):
-    post=get_object_or_404(Post,pk=pk)
 
-    #阅读量+1
-    post.increase_views()
+#处理具体文章内荣的视图函数
+#def detail(request,pk):
+#    post=get_object_or_404(Post,pk=pk)
 
-    post.body=markdown.markdown(post.body,
-                                extensions=[
-                                    'markdown.extensions.extra',
-                                    'markdown.extensions.codehilite',
-                                    'markdown.extensions.toc',
-                                    ])
-    form=CommentForm()
-    comment_list=post.comment_set.all()
-    context={'post':post,
-             'form':form,
-             'comment_list':comment_list
-             }
-    return render(request,'blog/detail.html',context=context)
+#    #阅读量+1
+#    post.increase_views()
+
+#    post.body=markdown.markdown(post.body,
+#                                extensions=[
+#                                    'markdown.extensions.extra',
+#                                    'markdown.extensions.codehilite',
+#                                    'markdown.extensions.toc',
+#                                    ])
+#    form=CommentForm()
+#    comment_list=post.comment_set.all()
+#    context={'post':post,
+#             'form':form,
+#             'comment_list':comment_list
+#             }
+#    return render(request,'blog/detail.html',context=context)
+
+#处理文章内容的类视图
+class PostDetailView(DetailView):
+    model=Post
+    template_name='blog/detail.html'
+    context_object_name='post'
+
+    def get(self,request,*args,**kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response=super(PostDetailView,self).get(request,*args,**kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        return response
+
+    def get_object(self,queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post=super(PostDetailView,self).get_object(queryset=None)
+        post.body=markdown.markdown(post.body,
+                                    extensions=[
+                                        'markdown.extensions.extra',
+                                        'markdown.extensions.codehilite',
+                                        'markdown.extensions.toc',
+                                        ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context=super(PostDetailView,self).get_context_data(**kwargs)
+        form=CommentForm()
+        comment_list=self.object.comment_set.all()
+        context.update({
+            'form':form,
+            'comment_list':comment_list
+            })
+        return context
+
+
 
 #处理归档的视图函数
 #def archives(request,year,month):
