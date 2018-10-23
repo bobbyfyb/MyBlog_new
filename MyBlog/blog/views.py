d3ca8ca60a0d7812e79e9016ca0bc65b31aa5286
Ayanami_Asuka <fyb0118@gmail.com>
2018-10-23 16:19:58 +0800

    添加文章搜索功能

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post,Category,Tag
 from comments.forms import CommentForm
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.db.models import Q
 import markdown
 import pygments
 # Create your views here.
@@ -274,3 +275,18 @@ class TagView(ListView):
     def get_queryset(self):
         tag=get_object_or_404(Tag,pk=self.kwargs.get('pk'))
         return super(TagView,self).get_queryset().filter(tags=tag)
+
+
+
+#处理搜索
+def search(request):
+    q=request.GET.get('q')
+    error_msg=' '
+
+    if not q:
+        error_msg="请输入关键词"
+        return render(request,'blog/index.html',{ 'error_msg':error_msg })
+
+    post_list=Post.objects.filter(Q(title__icontains=q)|Q(body__icontains=q))
+    return render(request,'blog/index.html',{'error_msg':error_msg,
+                                             'post_list':post_list})
